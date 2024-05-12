7a084b0e90f30e5206ef0ad8028b3c1a718a3745
exojade <38240183+exojade@users.noreply.github.com>
2024-05-13 00:06:31 +0800

    first commit

--- public/student_system/studentSpecific.php
+++ public/student_system/studentSpecific.php
@@ -73,6 +73,71 @@
 
 
 
+      <div class="modal fade" id="modalPaymentStudent">
+        <div class="modal-dialog ">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h4 class="modal-title">Add Payment (Student Module)</h4>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <form class="generic_form_files_trigger" role="form" enctype="multipart/form-data" data-url="subjects">
+              <div class="form-group">
+                    <label for="exampleInputEmail1">Balance</label>
+                    <input type="text" readonly value="13,760.00" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                  </div>
+
+              <div class="form-group">
+                    <label for="exampleInputEmail1">Date</label>
+                    <input type="date" readonly value="<?php echo(date("Y-m-d")); ?>" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                  </div>
+
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Mode of Payment</label>
+                    <input type="text" readonly value="ONLINE" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Bank Name</label>
+                    <select required name="gender" class="form-control select2">
+                          <option selected disabled value="">Filter Bank</option>
+                          <option value="">GCash</option>
+                          <option value="">BDO</option>
+                          <option value="">BPI</option>
+                        </select>
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Amount</label>
+                    <input type="text"  class="form-control" id="exampleInputEmail1" placeholder="0.00">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputFile">File input</label>
+                    <div class="input-group">
+                      <div class="custom-file">
+                        <input type="file" class="custom-file-input" id="exampleInputFile">
+                        <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                      </div>
+                      <div class="input-group-append">
+                        <span class="input-group-text">Upload</span>
+                      </div>
+                    </div>
+                  </div>
+        
+        
+            </div>
+            <div class="modal-footer justify-content-between">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              <button type="submit" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+          <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+      </div>
+
+
+
         <div class="row">
           <div class="col-md-12">
           <div class="row">
@@ -284,7 +349,11 @@
                     </div>
                     <div class="col-md-4">
                     <div class="form-group">
+                      <?php if($_SESSION["sunbeam_app"]["role"] == "admin"): ?>
                         <a href="#" data-toggle="modal" data-target="#modalPayment" class="btn btn-info ">Add Payment</a>
+                      <?php else: ?>
+                        <a href="#" data-toggle="modal" data-target="#modalPaymentStudent" class="btn btn-info ">Add Payment</a>
+                      <?php endif; ?>
                         <a href="#" data-toggle="modal" data-target="modalPayment" class="btn btn-success ">View SoA</a>
                       </div>
                     </div>
