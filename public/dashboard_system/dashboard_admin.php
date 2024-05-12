8a69d28429e0ff01b6935e04062c11034e3a7f13
exojade <38240183+exojade@users.noreply.github.com>
2024-05-12 23:12:06 +0800

    first commit

--- public/dashboard_system/dashboard_admin.php
+++ public/dashboard_system/dashboard_admin.php
@@ -37,11 +37,11 @@
           <div class="col-12 col-sm-6 col-md-3">
             <div class="info-box mb-3">
               <div class="info-box-content">
-                <span class="info-box-number">41,410</span>
-                <span class="info-box-text">INQUIRIES</span>
+                <span class="info-box-number">10</span>
+                <span class="info-box-text">Teachers</span>
 
               </div>
-              <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-question"></i></span>
+              <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-chalkboard-teacher"></i></span>
 
             </div>
           </div>
@@ -51,19 +51,19 @@
           <div class="col-12 col-sm-6 col-md-3">
             <div class="info-box mb-3">
               <div class="info-box-content">
-                <span class="info-box-number">760</span>
-                <span class="info-box-text">PENDING PATIENTS</span>
+                <span class="info-box-number">12</span>
+                <span class="info-box-text">Grade Sections</span>
               </div>
-              <span class="info-box-icon bg-success elevation-1"><i class="fas fa-paw"></i></span>
+              <span class="info-box-icon bg-success elevation-1"><i class="fas fa-school"></i></span>
             </div>
           </div>
           <div class="col-12 col-sm-6 col-md-3">
             <div class="info-box mb-3">
               <div class="info-box-content">
-                <span class="info-box-number">2,000</span>
-                <span class="info-box-text">COMPLETED ACTIVITIES</span>
+                <span class="info-box-number">2023-2024</span>
+                <span class="info-box-text">Current School Year</span>
               </div>
-              <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-check"></i></span>
+              <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-calendar"></i></span>
             </div>
           </div>
         </div>
