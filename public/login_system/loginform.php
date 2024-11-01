0304cf7680059f42f6d5924b9fcbe105859b7c4f
Brian Jade Garcia <tradebryant@gmail.com>
2024-11-01 16:24:10 +0800

    this pc

--- public/login_system/loginform.php
+++ public/login_system/loginform.php
@@ -1,102 +1,106 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="public/static_system/uploads\logocityvet.png">
-    <title>Admin Login</title>
-    <link rel="stylesheet" href="public/static_system/style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"> <!-- Font Awesome CSS -->
-
-    <!-- <link rel="stylesheet" href="AdminLTE/bower_components/bootstrap/dist/css/bootstrap.min.css"> -->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>HRIS</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.7 -->
+  <link rel="stylesheet" href="AdminLTE/bower_components/bootstrap/dist/css/bootstrap.min.css">
   <!-- Font Awesome -->
-  <!-- <link rel="stylesheet" href="AdminLTE/bower_components/font-awesome/css/font-awesome.min.css"> -->
+  <link rel="stylesheet" href="AdminLTE/bower_components/font-awesome/css/font-awesome.min.css">
   <!-- Ionicons -->
-  <!-- <link rel="stylesheet" href="AdminLTE/bower_components/Ionicons/css/ionicons.min.css"> -->
+  <link rel="stylesheet" href="AdminLTE/bower_components/Ionicons/css/ionicons.min.css">
   <!-- SweetAlert -->
   <link rel="stylesheet" href="AdminLTE/plugins/sweetalert/sweetalert2.min.css">
   <!-- Theme style -->
-  <!-- <link rel="stylesheet" href="AdminLTE/dist/css/AdminLTE.min.css"> -->
+  <link rel="stylesheet" href="AdminLTE/dist/css/AdminLTE.min.css">
   <!-- iCheck -->
-  <!-- <link rel="stylesheet" href="AdminLTE/plugins/iCheck/square/blue.css"> -->
-</head>
+  <link rel="stylesheet" href="AdminLTE/plugins/iCheck/square/blue.css">
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
 <style>
-  .google-btn {
-            background-color: #DB4437;
-            color: #FFFFFF;
-            border: none;
-            border-radius: 25px;
-            padding: 10px 20px;
-            font-size: 16px;
-            cursor: pointer;
-            display: inline-flex;
-            align-items: center;
-            text-decoration: none;
-        }
+.layer {
+    background-color: rgba(150, 150, 150, 0.7);
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    backdrop-filter: blur(5px);
+}
+  </style>
+  
+</head>
 
-        .google-icon {
-            margin-right: 10px;
-        }
-</style>
-
-
-<body>
-    <section>
-        <div class="imgBox">
-            <img src="resources\bg.jpg">
+<!-- <header class="main-header" style="background-color: #244D07;">
+    <nav class="navbar navbar-static-top" style="margin-left: 0px;">
+      <div class="container">
+        <div class="navbar-header">
+          <a href="#" class="navbar-brand">HUMAN RESOURCE INFORMATION SYSTEM</a>
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
+            <i class="fa fa-bars"></i>
+          </button>
         </div>
+      </div>
+      /.container-fluid
+    </nav>
+  </header> -->
 
-        <div class="contentBox">  
-            <div class="formBox">
-                <span>
-                    <center>
-                        <img src="public/static_system/uploads/logo.png" height="100" style="margin: 5px;">
-                    </center>
-                </span>
-            <br>
-                <h2>Login</h2>
-<!-- login form section start -->
-                <form id="login_form" class="generic_form_trigger" data-url="">
-                    <div class="inputBox">
-                            <span>Username</span>
-                            <input name="username" type="text" name="username" required>
-                        </div>
+<body class="hold-transition login-page" style="
+background-image: url('resources/bg_current.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+">
+<div class="layer">
+<div class="login-box" >
+  <div class="login-logo">
+    <img src="resources/logoNoWhite.png" width="200" height="200">
+  </div>
+  <div class="login-box-body" >
+    <h2 class="login-box-msg text-center">Student Information System</h2>
+    <form id="login_form" autocomplete="off">
+      <div class="form-group has-feedback">
+      <input type="text" class="form-control" placeholder="Username" name="username" required="required">
+        <span class="glyphicon glyphicon-user form-control-feedback"></span>
+      </div>
+      <div class="form-group has-feedback">
+        <input type="password" class="form-control" placeholder="Password" name="password" required="required">
+        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+      </div>
+   
+          <button type="submit" class="btn btn-info btn-block btn-flat">Sign In</button>
+    </form>
 
-                        <div class="inputBox">
-                            <span>Password</span>
-                            <input name="password" type="password" name="password" required>
-                        </div>
-
-                        <div class="showPass">  
-                            <label>
-                                <input type="checkbox">
-                                Show Password
-                            </label>
-                        </div>
-
-                        <div class="inputBox">
-                            <input type="submit" value="Sign in" name="">
-                        </div>
-                 
-<br>
-<br>
-
-<br>
-<br>
-                  
-                </form>
-<!-- login form section end -->
-            </div>
-        </div>
-    </section>
-    <script src="AdminLTE/bower_components/jquery/dist/jquery.min.js"></script>
-<!-- Bootstrap 3.3.7 -->
-<!-- <script src="AdminLTE/bower_components/bootstrap/dist/js/bootstrap.min.js"></script> -->
-<!-- iCheck -->
-<!-- <script src="AdminLTE/plugins/iCheck/icheck.min.js"></script> -->
-<!-- SweetAlert -->
+   <br>
+   <br>
+  </div>
+  <Br>
+  <!-- <p style="color:#fff;" class="text-center">Copyright @ <?php echo(date("Y")); ?> CHRMO</p> -->
+</div>
+</div>
+<script src="AdminLTE/bower_components/jquery/dist/jquery.min.js"></script>
+<script src="AdminLTE/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="AdminLTE/plugins/iCheck/icheck.min.js"></script>
 <script src="AdminLTE/plugins/sweetalert/sweetalert2.min.js"></script>
-    <?php require("public/login_system/loginJs.php"); ?>
-
+<script src="resources/validate.js"></script>
+<?php require("public/login_system/loginJs.php"); ?>
+<!-- <script src="public/login_system/login.js"></script> -->
+<script>
+  $(function () {
+    $('input').iCheck({
+      checkboxClass: 'icheckbox_square-blue',
+      radioClass: 'iradio_square-blue',
+      increaseArea: '20%' // optional
+    });
+  });
+</script>
 </body>
 </html>
